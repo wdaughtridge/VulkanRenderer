6922c70eae0144ad71ed85875aa15bc8c587cca7
Will <wdaughtridge@gmail.com>
2020-07-15 09:15:23 -0400

    git weirdness

---
+++ cmake-build-release/CMakeFiles/Renderer.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/main.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/main.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Camera.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Camera.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/CommandBuffer.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/CommandBuffer.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Framebuffer.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Framebuffer.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/GraphicsPipeline.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/GraphicsPipeline.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/IndexBuffer.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/IndexBuffer.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Instance.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Instance.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/LogicalDevice.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/LogicalDevice.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Model.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Model.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/PhysicalDevice.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/PhysicalDevice.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Renderer.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Renderer.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Semaphore.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Semaphore.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Shader.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Shader.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Swapchain.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Swapchain.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/UniformBuffer.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/UniformBuffer.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/VertexBuffer.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/VertexBuffer.cpp.o"
+  "/Users/wdaughtridge/CLionProjects/VulkanRenderer/src/Window.cpp" "/Users/wdaughtridge/CLionProjects/VulkanRenderer/cmake-build-release/CMakeFiles/Renderer.dir/src/Window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-release/CMakeFiles/Renderer.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Renderer.dir/main.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Camera.cpp.o"
+  "CMakeFiles/Renderer.dir/src/CommandBuffer.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Framebuffer.cpp.o"
+  "CMakeFiles/Renderer.dir/src/GraphicsPipeline.cpp.o"
+  "CMakeFiles/Renderer.dir/src/IndexBuffer.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Instance.cpp.o"
+  "CMakeFiles/Renderer.dir/src/LogicalDevice.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Model.cpp.o"
+  "CMakeFiles/Renderer.dir/src/PhysicalDevice.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Renderer.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Semaphore.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Shader.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Swapchain.cpp.o"
+  "CMakeFiles/Renderer.dir/src/UniformBuffer.cpp.o"
+  "CMakeFiles/Renderer.dir/src/VertexBuffer.cpp.o"
+  "CMakeFiles/Renderer.dir/src/Window.cpp.o"
+  "Renderer"
+  "Renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
